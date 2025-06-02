1bc2f58668d67428fb774e461bc589477176d038
federico-init <fefe.borrometi@gmail.com>
2025-06-02 12:26:36 +0200

    chore: exercise 1.7, 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import Statistics from "./components/Statistics";
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [totalFeedbacks, setTotalFeedbacks] = useState(0);
+  const [average, setAverage] = useState(0);
 
-  //button event handlers
+  // button event handlers
   const handleClickGood = () => {
     setGood((current) => current + 1);
+    setTotalFeedbacks((current) => current + 1);
   };
   const handleClickNeutral = () => {
     setNeutral((current) => current + 1);
+    setTotalFeedbacks((current) => current + 1);
   };
   const handleClickBad = () => {
     setBad((current) => current + 1);
+    setTotalFeedbacks((current) => current + 1);
   };
 
   return (
@@ -24,10 +30,19 @@ const App = () => {
       <Button onClick={handleClickGood} text="🙂" />
       <Button onClick={handleClickNeutral} text="😐" />
       <Button onClick={handleClickBad} text="☹️" />
-      <h1>Statistics</h1>
+      {/* <h1>Statistics</h1>
       <div>🙂 : {good}</div>
       <div>😐 : {neutral}</div>
       <div>☹️ : {bad}</div>
+      <div>Total: {totalFeedbacks}</div>
+      <div>Average score: {getAverageScore()}</div>
+      <div>Positive: {getPositivePercentage()}</div> */}
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        totalFeedbacks={totalFeedbacks}
+      />
     </>
   );
 };
---
+++ part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Statistics = ({ good, neutral, bad, totalFeedbacks }) => {
+  const getAverageScore = () => {
+    // the feedback values are: good 1, neutral 0, bad -1
+    // so we can get the total of good feedback and subtract bad feedbacks, ignoring the neutral ones
+    const averageScore = (good - bad) / totalFeedbacks;
+    return averageScore || 0;
+  };
+
+  const getPositivePercentage = () => {
+    const positivePercentage = (good * 100) / totalFeedbacks;
+    return positivePercentage || 0;
+  };
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      <div>🙂 : {good}</div>
+      <div>😐 : {neutral}</div>
+      <div>☹️ : {bad}</div>
+      <div>Total: {totalFeedbacks}</div>
+      <div>Average score: {getAverageScore()}</div>
+      <div>Positive: {getPositivePercentage()} %</div>
+    </>
+  );
+};
+
+export default Statistics;
