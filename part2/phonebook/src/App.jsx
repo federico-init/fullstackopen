c515610fa43c7b683e212e0d99521abe3db440d2
federico-init <fefe.borrometi@gmail.com>
2025-06-04 11:09:06 +0200

    chore: exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -12,14 +13,19 @@ const App = () => {
     } else {
       const personToAdd = {
         name: newName,
+        number: newNumber,
       };
       setPersons([...persons, personToAdd]);
       setNewName("");
+      setNewNumber();
     }
   };
 
   const handleChangeName = (event) => {
     setNewName(event.target.value);
+  };
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const isPersonRegistered = (name) => {
@@ -35,12 +41,22 @@ const App = () => {
           <input type="text" value={newName} onChange={handleChangeName} />
         </div>
         <div>
+          number:
+          <input
+            type="number"
+            value={newNumber}
+            onChange={handleChangeNumber}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
