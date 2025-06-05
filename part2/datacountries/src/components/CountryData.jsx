48cb495cb2701cbc1b7b83b75739ed8ecd7f8b29
federico-init <fefe.borrometi@gmail.com>
2025-06-06 00:51:00 +0200

    chore: submit part 2 exercises

--- part2/datacountries/src/components/CountryData.jsx
+++ part2/datacountries/src/components/CountryData.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+import countriesService from "../services/countries";
+
 const CountryData = ({ data }) => {
   const countryData = data[0];
+  const lat = countryData.latlng[0];
+  const lon = countryData.latlng[1];
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    countriesService
+      .getCountryWeather(lat, lon)
+      .then((response) => setWeatherData(response));
+  }, [data]);
 
   return (
     <div>
@@ -13,6 +26,13 @@ const CountryData = ({ data }) => {
         ))}
       </ul>
       <img src={countryData.flags.png} alt={countryData.flags.alt} />
+      <h2>Weather in {countryData.name.common}</h2>
+      <p>Temperature {weatherData?.main.temp} Celsius</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@2x.png`}
+        alt=""
+      />
+      <p>Wind {weatherData?.wind.speed} m/s</p>
     </div>
   );
 };
--- part2/datacountries/src/services/countries.js
+++ part2/datacountries/src/services/countries.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
 const BASE_URL = "https://studies.cs.helsinki.fi/restcountries/api/all";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const getCountries = () => {
   const request = axios.get(BASE_URL);
   return request.then((response) => response.data);
 };
 
-export default { getCountries };
+const getCountryWeather = (lat, lon) => {
+  const request = axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  );
+  return request.then((response) => response.data);
+};
+
+export default { getCountries, getCountryWeather };
