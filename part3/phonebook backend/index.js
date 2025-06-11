14224395a6a502a9f925ce33977617963c4e4a70
federico-init <fefe.borrometi@gmail.com>
2025-06-11 21:19:01 +0200

    chore: exercise 3.5

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -23,6 +25,10 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const generateId = () => {
+  return Math.floor(Math.random() * 999_999);
+};
 
 // default route
 app.get("/", (_, response) => {
@@ -64,6 +70,27 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+// POST
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "content is missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = [...persons, person];
+
+  response.json(person);
 });
 
 const PORT = 3001;
