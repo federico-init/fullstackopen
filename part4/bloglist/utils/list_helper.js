9142b22942c7ce7bcea80463e55f4a5b29b37569
federico-init <fefe.borrometi@gmail.com>
2025-06-27 15:04:44 +0200

    chore: exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,21 @@
+const logger = require("./logger")
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (!blogs.length) return 0
+
+  const reducer = (sum, blog) => {
+    logger.info(sum)
+    return sum + blog
+  }
+
+  return blogs.map((blog) => blog.likes).reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
