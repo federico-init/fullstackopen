5398b5e77de4da43b345c7a0b622f82d9ee9967a
federico-init <fefe.borrometi@gmail.com>
2025-06-27 12:13:14 +0200

    chore: exercise 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,57 +1,7 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const morgan = require("morgan")
+const app = require("./app")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-morgan.token("body", (req, _) => {
-  return JSON.stringify(req.body)
-})
-
-app.use(express.json())
-app.use(
-  morgan((tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-      tokens.body(req, res),
-    ].join(" ")
-  })
-)
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
